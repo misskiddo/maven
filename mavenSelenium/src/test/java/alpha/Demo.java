606e2961c6f851c9398858b5bd3b4d0e3fa8da47
Toñi <misskiddo@hotmail.com>
2020-03-11 17:01:00 -0400

    log4j

---
+++ mavenSelenium/src/test/java/alpha/Demo.java
@@ -0,0 +1,24 @@
+package alpha;
+
+import org.apache.logging.log4j.*;
+
+public class Demo {
+
+	private static Logger log = LogManager.getLogger(Demo.class.getName());
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("I am debugging");
+		
+		if(5>4) {
+			log.info("object is present");
+		}
+		
+			log.error("object is not present");
+			
+			log.fatal("this is fatal");
+
+	}
+
+}
